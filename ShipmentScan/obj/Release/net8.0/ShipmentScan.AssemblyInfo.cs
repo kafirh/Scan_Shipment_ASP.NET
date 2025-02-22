382cae7e8f2462cdeee15990138e5cd3d986432d
Moch. Irham Kafi Billah <irhamkafi22@gmail.com>
2025-02-22 15:36:36 +0700

    Memperbarui handling

--- ShipmentScan/obj/Release/net8.0/ShipmentScan.AssemblyInfo.cs
+++ ShipmentScan/obj/Release/net8.0/ShipmentScan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShipmentScan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7cf24f280e98565d202ebbe0b058119a005f89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShipmentScan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShipmentScan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
